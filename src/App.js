d2d8c1e44cab0b5700537c460a37b928b341d4d7
Alicia Olivares Canales <alicia93oc@gmail.com>
2025-07-28 21:36:27 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Swal from "sweetalert2";
 
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 import Projects from "./components/Projects";
 import TechnicalSkills from "./components/TechnicalSkills";
 import SSkillsEducation from "./components/SSkillsEducation";
@@ -13,124 +15,19 @@ import ExperienceCertifications from "./components/ExperienceCertifications";
 import WhoAmI from "./components/WhoAmI";
 import ContactMe from "./components/ContactMe";
 
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+
 function App() {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000, // optional settings like animation duration
-      once: true, // only animate once while scrolling down
-    });
-  }, []);
-
-  const handleClick = () => {
-    Swal.fire({
-      title: "Coming soon, stay tuned!",
-      width: 600,
-      padding: "3em",
-      color: "#F78FB3",
-    });
-  };
-
   return (
-    <>
-      <div className="main ">
-        <section id="about" className="about section">
-          <div
-            className="container section-title"
-            data-aos="fade-right"
-            data-aos-delay="10"
-          >
-            <div className="main-title">
-              <h1>Alicia Olivares Canales</h1>{" "}
-              <div className="buttons-container">
-                <button
-                  onClick={handleClick}
-                  style={{
-                    backgroundColor: "#6CA9E3",
-                    transition: "transform 0.3s ease-in-out",
-                    color: "white",
-                  }}
-                  className="btn blog-btn pyramid-btn"
-                >
-                  Check my blog
-                </button>
-                <button
-                  disabled
-                  onClick={handleClick}
-                  style={{
-                    backgroundColor: "#F78FB3",
-                    transition: "transform 0.3s ease-in-out",
-                    color: "white",
-                  }}
-                  id="English-locale"
-                  className="btn blog-btn pyramid-btn"
-                >
-                  EN
-                </button>
-                <button
-                  onClick={handleClick}
-                  style={{
-                    backgroundColor: "#7ED6A3",
-                    transition: "transform 0.3s ease-in-out",
-                    color: "white",
-                  }}
-                  className="btn blog-btn pyramid-btn"
-                >
-                  ES
-                </button>
-              </div>
-              <p>
-                Junior Full Stack self-taught looking for her opportunity to{" "}
-                <b>shine</b>!
-              </p>
-            </div>
-          </div>
-
-          <div className="container">
-            <div className="row align-items-center">
-              <ContactMe />
-              <WhoAmI />
-            </div>
-          </div>
-        </section>
-
-        <section id="resume" className="main-page">
-          <div
-            className="container section-title"
-            data-aos="zoom-in"
-            data-aos-delay="400"
-          >
-            <h1>Resume</h1>
-            <p>
-              Self-taught full-stack junior developer with a passion for coding
-              and a strong drive to build a career in the IT industry. Curious
-              and highly motivated, I’m seeking an opportunity to take on
-              challenges and grow both professionally and personally.
-            </p>
-          </div>
-          <div
-            className="container section-div"
-            data-aos="fade-up"
-            data-aos-delay="400"
-          >
-            <div className="row align-items-start">
-              <TechnicalSkills />
-              <Projects />
-            </div>
-          </div>
-          <div
-            className="container section-div"
-            data-aos="fade-up"
-            data-aos-delay="400"
-          >
-            <div className="row align-items-start ">
-              <SSkillsEducation />
-              <ExperienceCertifications />
-            </div>
-          </div>
-        </section>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
