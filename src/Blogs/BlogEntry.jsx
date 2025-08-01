cc4541c5cadd953d0ea5f6c1a7d0726dea9b58f3
Alicia Olivares Canales <alicia93oc@gmail.com>
2025-08-01 23:05:35 +0200

    finished phase1 of navbar

--- src/Blogs/BlogEntry.jsx
+++ src/Blogs/BlogEntry.jsx
@@ -125,6 +125,122 @@ const BlogEntry = [
           },
         ],
       },
+      {
+        month: "August",
+        items: [
+          {
+            date: "today",
+            moodBefore: moodBefore[1],
+            moodAfter: moodAfter[1],
+            title: "This is also a title",
+            body: "This sidebar layout also considers sticky and scrolling behaviors. The sidebar and bottom footer both appear fixed on larger screens. Only the content area is scrollable (when content height allows). On mobile screens, the horizontal sidebar remains fixed at the top (using sticky position), and the remaining content and footer sections scroll the entire page height. For this reason a small CSS change is need to make the height responsive...",
+          },
+          {
+            date: "today",
+            moodBefore: moodBefore[2],
+            moodAfter: moodAfter[1],
+
+            title: "this is a Title",
+            body: "This sidebar layout also considers sticky and scrolling behaviors. The sidebar and bottom footer both appear fixed on larger screens. Only the content area is scrollable (when content height allows). On mobile screens, the horizontal sidebar remains fixed at the top (using sticky position), and the remaining content and footer sections scroll the entire page height. For this reason a small CSS change is need to make the height responsive...",
+          },
+          {
+            date: "for real",
+            moodBefore: moodBefore[3],
+            moodAfter: moodAfter[1],
+
+            title: "This is also a title",
+            body: "This sidebar layout also considers sticky and scrolling behaviors. The sidebar and bottom footer both appear fixed on larger screens. Only the content area is scrollable (when content height allows). On mobile screens, the horizontal sidebar remains fixed at the top (using sticky position), and the remaining content and footer sections scroll the entire page height. For this reason a small CSS change is need to make the height responsive...",
+          },
+          {
+            date: "today",
+            moodBefore: moodBefore[1],
+            moodAfter: moodAfter[1],
+
+            title: "owooooooooooooooow",
+            body: "This sidebar layout also considers sticky and scrolling behaviors. The sidebar and bottom footer both appear fixed on larger screens. Only the content area is scrollable (when content height allows). On mobile screens, the horizontal sidebar remains fixed at the top (using sticky position), and the remaining content and footer sections scroll the entire page height. For this reason a small CSS change is need to make the height responsive...",
+          },
+          {
+            date: "today",
+            moodBefore: moodBefore[1],
+            moodAfter: moodAfter[1],
+
+            title: "This is also a title",
+            body: "This sidebar layout also considers sticky and scrolling behaviors. The sidebar and bottom footer both appear fixed on larger screens. Only the content area is scrollable (when content height allows). On mobile screens, the horizontal sidebar remains fixed at the top (using sticky position), and the remaining content and footer sections scroll the entire page height. For this reason a small CSS change is need to make the height responsive...",
+          },
+          {
+            date: "today",
+            moodBefore: moodBefore[1],
+            moodAfter: moodAfter[1],
+
+            title: "This is also a title",
+            body: "This sidebar layout also considers sticky and scrolling behaviors. The sidebar and bottom footer both appear fixed on larger screens. Only the content area is scrollable (when content height allows). On mobile screens, the horizontal sidebar remains fixed at the top (using sticky position), and the remaining content and footer sections scroll the entire page height. For this reason a small CSS change is need to make the height responsive...",
+          },
+          {
+            date: "today",
+            moodBefore: moodBefore[1],
+            moodAfter: moodAfter[1],
+
+            title: "This is also a title",
+            body: "This sidebar layout also considers sticky and scrolling behaviors. The sidebar and bottom footer both appear fixed on larger screens. Only the content area is scrollable (when content height allows). On mobile screens, the horizontal sidebar remains fixed at the top (using sticky position), and the remaining content and footer sections scroll the entire page height. For this reason a small CSS change is need to make the height responsive...",
+          },
+          {
+            date: "today",
+            moodBefore: moodBefore[1],
+            moodAfter: moodAfter[1],
+
+            title: "This is also a title",
+            body: "This sidebar layout also considers sticky and scrolling behaviors. The sidebar and bottom footer both appear fixed on larger screens. Only the content area is scrollable (when content height allows). On mobile screens, the horizontal sidebar remains fixed at the top (using sticky position), and the remaining content and footer sections scroll the entire page height. For this reason a small CSS change is need to make the height responsive...",
+          },
+          {
+            date: "today",
+            moodBefore: moodBefore[2],
+            moodAfter: moodAfter[1],
+
+            title: "this is a Title",
+            body: "This sidebar layout also considers sticky and scrolling behaviors. The sidebar and bottom footer both appear fixed on larger screens. Only the content area is scrollable (when content height allows). On mobile screens, the horizontal sidebar remains fixed at the top (using sticky position), and the remaining content and footer sections scroll the entire page height. For this reason a small CSS change is need to make the height responsive...",
+          },
+          {
+            date: "for real",
+            moodBefore: moodBefore[3],
+            moodAfter: moodAfter[1],
+
+            title: "This is also a title",
+            body: "This sidebar layout also considers sticky and scrolling behaviors. The sidebar and bottom footer both appear fixed on larger screens. Only the content area is scrollable (when content height allows). On mobile screens, the horizontal sidebar remains fixed at the top (using sticky position), and the remaining content and footer sections scroll the entire page height. For this reason a small CSS change is need to make the height responsive...",
+          },
+          {
+            date: "today",
+            moodBefore: moodBefore[1],
+            moodAfter: moodAfter[1],
+
+            title: "owooooooooooooooow",
+            body: "This sidebar layout also considers sticky and scrolling behaviors. The sidebar and bottom footer both appear fixed on larger screens. Only the content area is scrollable (when content height allows). On mobile screens, the horizontal sidebar remains fixed at the top (using sticky position), and the remaining content and footer sections scroll the entire page height. For this reason a small CSS change is need to make the height responsive...",
+          },
+          {
+            date: "today",
+            moodBefore: moodBefore[1],
+            moodAfter: moodAfter[1],
+
+            title: "This is also a title",
+            body: "This sidebar layout also considers sticky and scrolling behaviors. The sidebar and bottom footer both appear fixed on larger screens. Only the content area is scrollable (when content height allows). On mobile screens, the horizontal sidebar remains fixed at the top (using sticky position), and the remaining content and footer sections scroll the entire page height. For this reason a small CSS change is need to make the height responsive...",
+          },
+          {
+            date: "today",
+            moodBefore: moodBefore[1],
+            moodAfter: moodAfter[1],
+
+            title: "This is also a title",
+            body: "This sidebar layout also considers sticky and scrolling behaviors. The sidebar and bottom footer both appear fixed on larger screens. Only the content area is scrollable (when content height allows). On mobile screens, the horizontal sidebar remains fixed at the top (using sticky position), and the remaining content and footer sections scroll the entire page height. For this reason a small CSS change is need to make the height responsive...",
+          },
+          {
+            date: "today",
+            moodBefore: moodBefore[1],
+            moodAfter: moodAfter[1],
+
+            title: "This is also a title",
+            body: "This sidebar layout also considers sticky and scrolling behaviors. The sidebar and bottom footer both appear fixed on larger screens. Only the content area is scrollable (when content height allows). On mobile screens, the horizontal sidebar remains fixed at the top (using sticky position), and the remaining content and footer sections scroll the entire page height. For this reason a small CSS change is need to make the height responsive...",
+          },
+        ],
+      },
     ],
   },
 ];
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,124 +1,119 @@
 import "./Blog.css";
 
 import BlogEntry from "../Blogs/BlogEntry";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import Avatar from "../img/Avatar.png";
 
 const Blog = () => {
   const [blog, setBlog] = useState(BlogEntry[0].months[0].items[0]);
   const [isSidebarClosed, setIsSidebarClosed] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(null);
 
-  const nav1 = (
-    <nav id="navmenu" className="navmenu">
-      <button id="toggle-btn">
-        <i class="bi bi-list"></i>
-      </button>
-      <div className="nav-head">
-        {" "}
-        <a href="/" className="">
-          <img src={Avatar} alt="Avatar" className="img-small" />{" "}
-          <h1 className="navbar-name">Alicia O.</h1>
-        </a>
-        <div class="social-links">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="mailto:alicia.olivares.canales@gmail.com"
-            class="instagram"
-          >
-            <i class="bi bi-envelope-at"></i>
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/Alicia-OC"
-            class="google-plus"
-          >
-            <i class="bi bi-github"></i>
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.linkedin.com/in/alicia-olivares-canales-b0a629141/"
-            class="linkedin"
-          >
-            <i class="bi bi-linkedin"></i>
-          </a>
-        </div>
-      </div>
-      <div className="nav-blog-entries">
-        {" "}
-        <ul>
-          {BlogEntry.map((yearGroup) => (
-            <div key={yearGroup.year}>
-              <p>{yearGroup.year}</p>
-              {yearGroup.months.map((monthGroup) => (
-                <div key={monthGroup.month}>
-                  <li>
-                    <button className="dropdown-btn">
-                      <span>{monthGroup.month}</span>
-                    </button>
-                    <ul className="sub-menu">
-                      {monthGroup.items.map((item) => (
-                        <li key={item.key}>
-                          <span
-                            className="blog-li"
-                            role="button"
-                            tabIndex={0}
-                            onClick={() => setBlog(item)}
-                            onKeyDown={(e) =>
-                              e.key === "Enter" && setBlog(item)
-                            }
-                          >
-                            {item.date}
-                          </span>
-                        </li>
-                      ))}
-                    </ul>
-                  </li>
-
-                  <h3>{monthGroup.month}</h3>
-                  <ul>
-                    {monthGroup.items.map((item) => (
-                      <li key={item.key}>
-                        <span
-                          className="blog-li"
-                          role="button"
-                          tabIndex={0}
-                          onClick={() => setBlog(item)}
-                          onKeyDown={(e) => e.key === "Enter" && setBlog(item)}
-                        >
-                          {item.date}
-                        </span>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ))}
-            </div>
-          ))}
-        </ul>
-      </div>
-    </nav>
-  );
-
-  const toggleSUbMenu = (button) => {
-    setIsOpen((prev) => !prev);
+  const toggleSubMenu = (button) => {
+    setIsOpen((prev) => (prev === button ? null : button));
   };
 
   const toggleSidebar = () => {
     setIsSidebarClosed((prev) => !prev);
   };
 
+  const isOpenMap = () => {
+    return BlogEntry.map((yearGroup) => (
+      <div key={yearGroup.year}>
+        <p className="navbar-group-year">{yearGroup.year}</p>
+        {yearGroup.months.map((monthGroup) => (
+          <div key={monthGroup.month}>
+            <li>
+              <button
+                onClick={() => toggleSubMenu(monthGroup.month)}
+                className={`dropdown-btn ${
+                  isOpen === monthGroup.month ? "rotate" : ""
+                }`}
+              >
+                <i className="bi bi-card-text"></i>
+                <span>{monthGroup.month}</span>
+                <i className="bi bi-caret-down"></i>
+              </button>
+
+              {isOpen === monthGroup.month && (
+                <ul className="sub-menu show">
+                  {monthGroup.items.map((item, index) => (
+                    <li key={item.key}>
+                      <span
+                        className="blog-li"
+                        role="button"
+                        tabIndex={0}
+                        onClick={() => setBlog(item)}
+                        onKeyDown={(e) => e.key === "Enter" && setBlog(item)}
+                      >
+                        #{index} {item.date}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          </div>
+        ))}
+      </div>
+    ));
+  };
+
+  const isClosedMap = () => {
+    return BlogEntry.map((yearGroup) => (
+      <div key={yearGroup.year} className="navbar-small">
+        <p className="navbar-group-year-small">
+          '
+          {yearGroup.year.toString().charAt(2) +
+            yearGroup.year.toString().charAt(3)}
+        </p>
+
+        {yearGroup.months.map((monthGroup) => (
+          <div key={monthGroup.month}>
+            <li>
+              <button
+                onClick={() => toggleSubMenu(monthGroup.month)}
+                className={`dropdown-btn ${
+                  isOpen !== monthGroup.month ? "" : "small"
+                }`}
+              >
+                <span>{monthGroup.month.charAt(0)}</span>
+              </button>
+
+              {isOpen === monthGroup.month && (
+                <ul className="sub-menu-small show">
+                  {monthGroup.items.map((item, index) => (
+                    <li key={item.key}>
+                      <span
+                        className="blog-li"
+                        role="button"
+                        tabIndex={0}
+                        onClick={() => setBlog(item)}
+                        onKeyDown={(e) => e.key === "Enter" && setBlog(item)}
+                      >
+                        #{index}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          </div>
+        ))}
+      </div>
+    ));
+  };
+
   return (
     <div className="main-blog-container">
       <div>
-        {" "}
         <nav
           id="sidebar"
           className={`sidebar ${isSidebarClosed ? "close" : ""}`}
+          role="navigation"
+          aria-label="Blog side navbar"
+          aria-hidden={isSidebarClosed}
         >
           <li>
             <span>
@@ -126,130 +121,86 @@ const Blog = () => {
                 id="toggle-btn"
                 className={`toggle-btn ${isSidebarClosed ? "rotate" : ""}`}
                 onClick={(e) => toggleSidebar(e.currentTarget)}
+                aria-label={isSidebarClosed ? "Open navbar" : "Close navbar"}
+                aria-expanded={!isSidebarClosed}
+                aria-controls="sidebar"
               >
-                {" "}
                 <span>
-                  {" "}
-                  <i class="bi bi-caret-left-fill"></i>
+                  <i className="bi bi-caret-left-fill"></i>
                 </span>
               </button>
             </span>
           </li>
 
           <div className="nav-head">
-            {" "}
             <a href="/" className="">
-              <img src={Avatar} alt="Avatar" className="img-small" />{" "}
-              <h1 className="navbar-name">Alicia O.</h1>
+              <img
+                src={Avatar}
+                alt="pic profile"
+                className={`img-small ${isSidebarClosed ? "close" : ""}`}
+              />{" "}
+              <h1 className={`navbar-name ${isSidebarClosed ? "close" : ""}`}>
+                Alicia O.
+              </h1>
+              <h1
+                className={`navbar-name-small ${
+                  isSidebarClosed ? "" : "close"
+                }`}
+              >
+                A
+              </h1>
             </a>
-            <div class="social-links">
+            <div className="social-links">
               <a
                 target="_blank"
                 rel="noopener noreferrer"
                 href="mailto:alicia.olivares.canales@gmail.com"
-                class="instagram"
+                className="social-link email"
+                aria-label="Send mail to Alicia Olivares"
               >
-                <i class="bi bi-envelope-at"></i>
+                <i className="bi bi-envelope-at"></i>
               </a>
               <a
                 target="_blank"
                 rel="noopener noreferrer"
                 href="https://github.com/Alicia-OC"
-                class="google-plus"
+                className="social-link github"
+                aria-label="Go to Github profile"
               >
-                <i class="bi bi-github"></i>
+                <i className="bi bi-github"></i>
               </a>
               <a
                 target="_blank"
                 rel="noopener noreferrer"
                 href="https://www.linkedin.com/in/alicia-olivares-canales-b0a629141/"
-                class="linkedin"
+                className="social-link linkedin"
+                aria-label="Go to LinkedIn profile"
               >
-                <i class="bi bi-linkedin"></i>
+                <i className="bi bi-linkedin"></i>
               </a>
             </div>
           </div>
-          <div className="nav-blog-entries">
-            {" "}
-            <ul>
-              {BlogEntry.map((yearGroup) => (
-                <div key={yearGroup.year}>
-                  <p>{yearGroup.year}</p>
-                  {yearGroup.months.map((monthGroup) => (
-                    <div key={monthGroup.month}>
-                      <li>
-                        <button
-                          onClick={(e) => toggleSUbMenu(e.currentTarget)}
-                          className={`dropdown-btn ${isOpen ? "rotate" : ""}`}
-                        >
-                          <i class="bi bi-card-text"></i>
-                          <span>{monthGroup.month}</span>
-                          <i class="bi bi-caret-down"></i>
-                        </button>
-                        <ul className="sub-menu show">
-                          <div>
-                            {isOpen && (
-                              <div className="sub-menu show">
-                                {monthGroup.items.map((item) => (
-                                  <li key={item.key}>
-                                    <span
-                                      className="blog-li"
-                                      role="button"
-                                      tabIndex={0}
-                                      onClick={() => setBlog(item)}
-                                      onKeyDown={(e) =>
-                                        e.key === "Enter" && setBlog(item)
-                                      }
-                                    >
-                                      {item.date}
-                                    </span>
-                                  </li>
-                                ))}
-                              </div>
-                            )}
-                          </div>
-                        </ul>
-                      </li>
 
-                      <h3>{monthGroup.month}</h3>
-                      <ul>
-                        {monthGroup.items.map((item) => (
-                          <li key={item.key}>
-                            <span
-                              className="blog-li"
-                              role="button"
-                              tabIndex={0}
-                              onClick={() => setBlog(item)}
-                              onKeyDown={(e) =>
-                                e.key === "Enter" && setBlog(item)
-                              }
-                            >
-                              {item.date}
-                            </span>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  ))}
-                </div>
-              ))}
-            </ul>
+          <div className="blogs-listed">
+            {isSidebarClosed ? isClosedMap() : <ul>{isOpenMap()}</ul>}
           </div>
         </nav>
       </div>
 
-      <section className="main-blogs-section">
-        {" "}
-        <h1>Blog # {BlogEntry.length + 1}</h1>
-        <div className="blog-header">
-          {" "}
-          <p>
+      <section
+        className="main-blogs-section"
+        role="main"
+        aria-labelledby="blog-section"
+      >
+        <h1 id="blog-title">Blog # {BlogEntry.length + 1}</h1>
+        <div className="blog-header" role="contentinfo">
+          <p className="blog-subtitle">
             Date: {blog.date}, Mood before: {blog.moodBefore} Mood after:{" "}
             {blog.moodAfter}
           </p>
         </div>
-        <h3>{blog.title}</h3>
-        <p>{blog.body}</p>
+        <h3 className="blog-title">{blog.title}</h3>
+        <p className="blog-body">{blog.body}</p>
       </section>
     </div>
   );
