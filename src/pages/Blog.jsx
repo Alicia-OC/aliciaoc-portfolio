b4e5a292de4ceafcc30e679d5acb34431539a1ef
Alicia Olivares Canales <alicia93oc@gmail.com>
2025-08-02 14:03:08 +0200

    navbar finished except toggle button

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -20,14 +20,14 @@ const Blog = () => {
 
   const isOpenMap = () => {
     return BlogEntry.map((yearGroup) => (
-      <div key={yearGroup.year}>
-        <p className="navbar-group-year">{yearGroup.year}</p>
+      <div className="open-navbar" key={yearGroup.year}>
+        <h4 className="navbar-group-year">{yearGroup.year}</h4>
         {yearGroup.months.map((monthGroup) => (
           <div key={monthGroup.month}>
             <li>
               <button
                 onClick={() => toggleSubMenu(monthGroup.month)}
-                className={`dropdown-btn ${
+                className={`dropdown-btn  ${
                   isOpen === monthGroup.month ? "rotate" : ""
                 }`}
               >
@@ -64,9 +64,7 @@ const Blog = () => {
     return BlogEntry.map((yearGroup) => (
       <div key={yearGroup.year} className="navbar-small">
         <p className="navbar-group-year-small">
-          '
-          {yearGroup.year.toString().charAt(2) +
-            yearGroup.year.toString().charAt(3)}
+          '{yearGroup.year.toString().substr(2, 3)}
         </p>
 
         {yearGroup.months.map((monthGroup) => (
@@ -74,11 +72,9 @@ const Blog = () => {
             <li>
               <button
                 onClick={() => toggleSubMenu(monthGroup.month)}
-                className={`dropdown-btn ${
-                  isOpen !== monthGroup.month ? "" : "small"
-                }`}
+                className={`dropdown-btn small`}
               >
-                <span>{monthGroup.month.charAt(0)}</span>
+                <span>{monthGroup.month.substr(0, 3)}</span>
               </button>
 
               {isOpen === monthGroup.month && (
@@ -108,6 +104,21 @@ const Blog = () => {
   return (
     <div className="main-blog-container">
       <div>
+        <span>
+          <button
+            id="toggle-btn"
+            className={`toggle-btn ${isSidebarClosed ? "rotate" : ""}`}
+            onClick={(e) => toggleSidebar(e.currentTarget)}
+            aria-label={isSidebarClosed ? "Open navbar" : "Close navbar"}
+            aria-expanded={!isSidebarClosed}
+            aria-controls="sidebar"
+          >
+            <span>
+              <i className="bi bi-caret-left-fill"></i>
+            </span>
+          </button>
+        </span>
+
         <nav
           id="sidebar"
           className={`sidebar ${isSidebarClosed ? "close" : ""}`}
@@ -115,74 +126,59 @@ const Blog = () => {
           aria-label="Blog side navbar"
           aria-hidden={isSidebarClosed}
         >
-          <li>
-            <span>
-              <button
-                id="toggle-btn"
-                className={`toggle-btn ${isSidebarClosed ? "rotate" : ""}`}
-                onClick={(e) => toggleSidebar(e.currentTarget)}
-                aria-label={isSidebarClosed ? "Open navbar" : "Close navbar"}
-                aria-expanded={!isSidebarClosed}
-                aria-controls="sidebar"
-              >
-                <span>
-                  <i className="bi bi-caret-left-fill"></i>
-                </span>
-              </button>
-            </span>
-          </li>
-
-          <div className="nav-head">
-            <a href="/" className="">
-              <img
-                src={Avatar}
-                alt="pic profile"
-                className={`img-small ${isSidebarClosed ? "close" : ""}`}
-              />{" "}
-              <h1 className={`navbar-name ${isSidebarClosed ? "close" : ""}`}>
-                Alicia O.
-              </h1>
-              <h1
-                className={`navbar-name-small ${
-                  isSidebarClosed ? "" : "close"
-                }`}
-              >
-                A
-              </h1>
-            </a>
-            <div className="social-links">
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="mailto:alicia.olivares.canales@gmail.com"
-                className="social-link email"
-                aria-label="Send mail to Alicia Olivares"
-              >
-                <i className="bi bi-envelope-at"></i>
+          <div className="sidebar-content">
+            {" "}
+            <div className="nav-head">
+              <a href="/" className="got-to-homepage">
+                <img
+                  src={Avatar}
+                  alt="pic profile"
+                  className={`img-small ${isSidebarClosed ? "close" : ""}`}
+                />{" "}
+                <h1 className={`navbar-name ${isSidebarClosed ? "close" : ""}`}>
+                  Alicia O.
+                </h1>
+                <h1
+                  className={`navbar-name-small ${
+                    isSidebarClosed ? "" : "close"
+                  }`}
+                >
+                  A
+                </h1>
               </a>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://github.com/Alicia-OC"
-                className="social-link github"
-                aria-label="Go to Github profile"
-              >
-                <i className="bi bi-github"></i>
-              </a>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.linkedin.com/in/alicia-olivares-canales-b0a629141/"
-                className="social-link linkedin"
-                aria-label="Go to LinkedIn profile"
-              >
-                <i className="bi bi-linkedin"></i>
-              </a>
+              <div className="social-links">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="mailto:alicia.olivares.canales@gmail.com"
+                  className="social-link email"
+                  aria-label="Send mail to Alicia Olivares"
+                >
+                  <i className="bi bi-envelope-at"></i>
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/Alicia-OC"
+                  className="social-link github"
+                  aria-label="Go to Github profile"
+                >
+                  <i className="bi bi-github"></i>
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.linkedin.com/in/alicia-olivares-canales-b0a629141/"
+                  className="social-link linkedin"
+                  aria-label="Go to LinkedIn profile"
+                >
+                  <i className="bi bi-linkedin"></i>
+                </a>
+              </div>
             </div>
-          </div>
-
-          <div className="blogs-listed">
-            {isSidebarClosed ? isClosedMap() : <ul>{isOpenMap()}</ul>}
+            <div className="blogs-listed">
+              {isSidebarClosed ? isClosedMap() : <ul>{isOpenMap()}</ul>}
+            </div>
           </div>
         </nav>
       </div>
